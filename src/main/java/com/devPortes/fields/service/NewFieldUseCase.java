7f2bffa0f2fabe961bcfc4be6bc0e5b03601bc24
Camilo <bermeocamilo12@gmail.com>
2026-09-10 13:23:50 -0500

    feat(application):configure application-dev because show sensitive information

--- src/main/java/com/devPortes/fields/service/NewFieldUseCase.java
+++ src/main/java/com/devPortes/fields/service/NewFieldUseCase.java
@@ -36,7 +36,7 @@ public class NewFieldUseCase implements INewFieldUseCase {
         List<String> urlPictures = dto.pictures().stream()
                 .map(iCloudinaryClient::saveImg)
                 .toList();
- */
+*/
         for(int i=0; i < dto.pictures().size() ;i++){
             urlImg = iCloudinaryClient.saveImg(dto.pictures().get(i));
             urlPictures.add(urlImg); }
