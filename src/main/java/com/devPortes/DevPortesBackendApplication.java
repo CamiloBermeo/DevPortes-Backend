8b11e26c7a3c5bd7fadd0fdcccca88d66e629dad
Camilo <bermeocamilo12@gmail.com>
2026-09-17 18:00:07 -0500

    bug(SpringApplication): exclude userdetailsmanager because this generate automaticaly credentials

--- src/main/java/com/devPortes/DevPortesBackendApplication.java
+++ src/main/java/com/devPortes/DevPortesBackendApplication.java
@@ -2,8 +2,10 @@ package com.devPortes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =UserDetailsServiceAutoConfiguration.class)
 public class DevPortesBackendApplication {
 
 	public static void main(String[] args) {
